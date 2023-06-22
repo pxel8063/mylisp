36b1f6cd80f4d9c55941e3e56dd9748f6274928e
github <83626280+pxel8063@users.noreply.github.com>
2023-06-22 19:33:50 +0900

    use buttercup idiom

--- test/mylisp-test.el
+++ test/mylisp-test.el
@@ -25,11 +25,11 @@
 	      (should (= 1 1))))
 
 (describe "tramp-remote-path"
+	  :var ((tramp-default-remote-path 'tramp-default-remote-path))
 	  (it "can build the tramp-remote-path as desired"
-	      (let* ((tramp-default-remote-path 'tramp-default-remote-path))
-		(should (equal (sf/add-nixos-path-to-tramp-remote-path)
+	      (expect (equal (sf/add-nixos-path-to-tramp-remote-path)
 (list "/run/current-system/sw/bin" "/nix/var/nix/profiles/default/bin" "/etc/profiles/per-user/cosmic/bin" "/home/cosmic/.nix-profile/bin" "/run/wrappers/bin" "/home/cosmic/.local/bin" tramp-default-remote-path "/bin" "/usr/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin" "/local/bin" "/local/freeware/bin" "/local/gnu/bin" "/usr/freeware/bin" "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin" "/opt/sbin" "/opt/local/bin")
-			       )))))
+			       ))))
 
 (ert-deftest addition-test ()
   (should (= 1 1)))
