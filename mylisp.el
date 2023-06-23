ce13229a5ff3ee03572af2404f4018515bfe889b
github <83626280+pxel8063@users.noreply.github.com>
2023-06-23 20:43:11 +0900

    increment version number to 0.0.7

--- mylisp.el
+++ mylisp.el
@@ -3,7 +3,7 @@
 ;; Copyright (C)   2022 pxel8063
 
 ;; Author:     pxel8063 <pxel8063@gmail.com>
-;; Version:    0.0.6
+;; Version:    0.0.7
 ;; Keywords:   lisp
 ;; Package-Requires: ((emacs "27.1") (org "9.5") (org-roam "2.2.2"))
 
