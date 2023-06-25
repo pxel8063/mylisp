e536d3f8a37c7257da43b6293d5d28e59991ec06
github <83626280+pxel8063@users.noreply.github.com>
2023-06-25 20:15:27 +0900

    test fix (forget to change upon changing naming convention)

--- test/mylisp-test.el
+++ test/mylisp-test.el
@@ -27,7 +27,7 @@
 (describe "tramp-remote-path"
 	  :var ((tramp-default-remote-path 'tramp-default-remote-path))
 	  (it "can build the tramp-remote-path as desired"
-	      (expect (sf/add-nixos-path-to-tramp-remote-path) :to-equal
+	      (expect (mylisp-add-nixos-path-to-tramp-remote-path) :to-equal
 (list "/run/current-system/sw/bin" "/nix/var/nix/profiles/default/bin" "/etc/profiles/per-user/cosmic/bin" "/home/cosmic/.nix-profile/bin" "/run/wrappers/bin" "/home/cosmic/.local/bin" tramp-default-remote-path "/bin" "/usr/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin" "/local/bin" "/local/freeware/bin" "/local/gnu/bin" "/usr/freeware/bin" "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin" "/opt/sbin" "/opt/local/bin")
 			       )))
 
@@ -37,7 +37,7 @@
 (ert-deftest nixos ()
    (let* ((tramp-default-remote-path 'tramp-default-remote-path))
      (should
-      (equal (sf/add-nixos-path-to-tramp-remote-path)
+      (equal (mylisp-add-nixos-path-to-tramp-remote-path)
 (list "/run/current-system/sw/bin" "/nix/var/nix/profiles/default/bin" "/etc/profiles/per-user/cosmic/bin" "/home/cosmic/.nix-profile/bin" "/run/wrappers/bin" "/home/cosmic/.local/bin" tramp-default-remote-path "/bin" "/usr/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin" "/local/bin" "/local/freeware/bin" "/local/gnu/bin" "/usr/freeware/bin" "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin" "/opt/sbin" "/opt/local/bin")
 			       ))))
 
