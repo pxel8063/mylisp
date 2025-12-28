375a8f0773b6ddefd9c5222793ce41619b4996cf
github <83626280+pxel8063@users.noreply.github.com>
2025-12-28 22:36:11 +0900

    bugfix
    * mylisp.el (mylisp-set-ssh-auth-sock): setting SSH_AUTH_SOCK appropriately,
    expand-file-name is needed.

--- mylisp.el
+++ mylisp.el
@@ -45,7 +45,8 @@
 (defun mylisp-set-ssh-auth-sock ()
   "Set a selected file name as SSH_AUTH_SOCK environmental variable"
   (interactive)
-  (setenv "SSH_AUTH_SOCK" (read-file-name "SSH agent socket: " mylisp-ssh-socket-directory)))
+  (setenv "SSH_AUTH_SOCK" (expand-file-name
+			   (read-file-name "SSH agent socket: " mylisp-ssh-socket-directory))))
 
 (defun mylisp-add-ssh-agent-to-tramp ()
   "Forward agent for ssh connections.
